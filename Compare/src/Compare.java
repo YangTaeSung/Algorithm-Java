7ddcb665b692aff8761438f611de17ecdddb6db2
YangTaeSung <tsy0668@gmail.com>
2020-04-03 23:35:34 +0900

    Add compare

---
+++ Compare/src/Compare.java
@@ -0,0 +1,20 @@
+
+public class Compare {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 1;
+		int b = 2;
+		
+		if(a > b) {
+			System.out.println(">");
+		} else if(a < b) {
+			System.out.println("<");
+		} else {
+			System.out.println("==");
+		}
+		
+	}
+
+}
