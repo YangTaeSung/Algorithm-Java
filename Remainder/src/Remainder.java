c7932bcff35d94660f9463dde04f01bb7d145945
YangTaeSung <tsy0668@gmail.com>
2020-04-03 19:39:26 +0900

    Add remainder calculaton

---
+++ Remainder/src/Remainder.java
@@ -0,0 +1,18 @@
+
+public class Remainder {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 5;
+		int b = 8;
+		int c = 4;
+		
+		System.out.println((a + b) % c);
+		System.out.println((a % c) + (b % c) % c);
+		System.out.println((a * b) % c);
+		System.out.println((a % c) * (b % c) % c);
+		
+	}
+
+}
