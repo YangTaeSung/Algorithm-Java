034f9d7c837ae54ee68db74127c9121c9c647e3e
YangTaeSung <tsy0668@gmail.com>
2020-04-03 18:27:15 +0900

    Add the output making cat

--- OutputTest/src/PrintTest.java
+++ OutputTest/src/PrintTest.java
@@ -5,9 +5,18 @@ public class PrintTest {
 		// TODO Auto-generated method stub
 
 		System.out.println("Hello World!");
-		
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
 		System.out.println("강한친구 대한육군");
 		System.out.println("강한친구 대한육군");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("");
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
 	}
 
 }
