1eddc604d4bd9bf7d1de7691fa8326a73af9ab96
YangTaeSung <tsy0668@gmail.com>
2020-04-04 19:19:38 +0900

    Add star iteration

---
+++ StarOutput/src/StarOutput.java
@@ -0,0 +1,36 @@
+import java.io.BufferedWriter;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+
+public class StarOutput {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		System.out.println("n을 입력하세요");
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		for(int j = 1; j <= n; j++ ) {
+			for(int i = 0; i < j; i++) {
+				bw.write("*");
+			}
+			bw.write("\n");
+		}
+		
+		bw.flush();
+		bw.close();
+		
+		
+		
+		
+		
+		
+	}
+
+}
