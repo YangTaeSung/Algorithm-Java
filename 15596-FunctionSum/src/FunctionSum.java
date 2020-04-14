157f5a851e6c6d3237fc3240c56eba76c3736fdf
YangTaeSung <tsy0668@gmail.com>
2020-04-14 23:37:18 +0900

    Add buffered writer and reader

--- 15596-FunctionSum/src/FunctionSum.java
+++ 15596-FunctionSum/src/FunctionSum.java
@@ -1,9 +1,13 @@
+import java.io.*;
 
 public class FunctionSum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
 	}
 
 }
