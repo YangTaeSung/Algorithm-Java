da59ecc22284f09c388d3024062dfc587a8d799a
YangTaeSung <tsy0668@gmail.com>
2020-04-17 23:41:13 +0900

    Add buffered writing test

--- 15596-FunctionSum/src/FunctionSum.java
+++ 15596-FunctionSum/src/FunctionSum.java
@@ -2,12 +2,15 @@ import java.io.*;
 
 public class FunctionSum {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		bw.write("output test");
+		bw.flush();
+		bw.close();
 	}
 
 }
