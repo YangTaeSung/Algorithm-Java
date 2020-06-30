241cbd675d22284a0f55b65b6d7df90659e794d2
YangTaeSung <tsy0668@gmail.com>
2020-07-01 03:47:36 +0900

    Add DFS practice at programmers

---
+++ Programmers-DP1/src/Exe.java
@@ -0,0 +1,12 @@
+
+public class Exe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int N = 2;
+		int number = 11;
+		Solution s = new Solution();
+		System.out.println(s.solution(N, number));
+	}
+
+}
