8afd620a443a47161b14ab0b589c8e5bd528ea8b
YangTaeSung <tsy0668@gmail.com>
2020-07-03 23:41:33 +0900

    Add main class

---
+++ Programmers-DFSBFS1/src/Exe.java
@@ -0,0 +1,12 @@
+
+public class Exe {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int numbers[] = {1,1,1,1};
+		int target = 3;
+		Solution s = new Solution();
+		System.out.println(s.solution(numbers, target));
+	}
+
+}
