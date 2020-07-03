e7ec8aa5057e87ace5a6d0493c5e0c92501fd3df
YangTaeSung <tsy0668@gmail.com>
2020-07-03 23:55:24 +0900

    Fix solution code

--- Programmers-DFSBFS1/src/Solution.java
+++ Programmers-DFSBFS1/src/Solution.java
@@ -2,16 +2,20 @@
 public class Solution {
 	public int solution(int[] numbers, int target) {
         int answer = 0;
+        int num = 0;
         answer = dfs(numbers, 0, 0, target);
         return answer;
     }
     
     private int dfs(int[] numbers, int node, int sum, int target){
         if(node == numbers.length){
-            if(sum==target)
-                return 1;
+            if(sum==target) {
+                //num = num+1;
+            	return 1;
+            }
             return 0;
         }
+        //System.out.println(num);
         return dfs(numbers, node+1, sum + numbers[node], target) 
         	 + dfs(numbers, node+1, sum - numbers[node], target);
     }
