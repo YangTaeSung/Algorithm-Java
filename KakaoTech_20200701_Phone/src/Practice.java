e570dc8f0a9c1f4384834479c9135cf686861dc1
YangTaeSung <tsy0668@gmail.com>
2020-09-10 04:08:05 +0900

    Add Kakao tech algorithm - phone

---
+++ KakaoTech_20200701_Phone/src/Practice.java
@@ -0,0 +1,13 @@
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String answer = "";
+		String result = answer + "L";
+		
+		System.out.println(result);
+
+	}
+
+}
