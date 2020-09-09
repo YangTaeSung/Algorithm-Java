68026099dd3f7c66d253f5f423166a8c62ea873d
YangTaeSung <tsy0668@gmail.com>
2020-09-09 23:35:36 +0900

    Fix comments

--- 15596-FunctionSum/src/FunctionSum.java
+++ 15596-FunctionSum/src/FunctionSum.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class FunctionSum {
 
 	public static void main(String[] args) throws IOException {
-		// TODO Auto-generated method stub
+		// TODO Auto-generated method stub //
 
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -11,7 +11,7 @@ public class FunctionSum {
 		bw.write("output test \n");
 		bw.flush();
 		bw.close();
-		
+	
 		String hello = br.readLine();
 		System.out.println("hello");
 		System.out.println("world");
