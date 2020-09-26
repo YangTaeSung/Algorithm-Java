547a0b418a16486c4aa1fead9cd219a030202278
YangTaeSung <tsy0668@gmail.com>
2020-09-26 18:43:36 +0900

    Add project

---
+++ Programmers-Stack1/src/Solution.java
@@ -0,0 +1,23 @@
+/*
+ * 
+ * 문제가 거지같아 그냥 그런갑다 해. 
+ * 
+ */
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length]; // 배열은 크기가 정해져있다. 설정해줘야해
+        
+        for(int i = 0; i < prices.length; i++) {
+            int second = 1;
+            for(int j = i+1; j < prices.length-1; j++) {
+                if(prices[i] <= prices[j]) second++;
+                else break;
+            }
+            answer[i] = second;
+        }
+        answer[prices.length-1] = 0;
+        answer[prices.length-2] = 1;
+        return answer;
+    }
+}
