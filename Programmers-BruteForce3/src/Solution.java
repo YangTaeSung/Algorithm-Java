2d2859a9f230ec72260cb3828ba96abfee2748be
YangTaeSung <tsy0668@gmail.com>
2020-10-09 18:17:51 +0900

    Fix code final

--- Programmers-BruteForce3/src/Solution.java
+++ Programmers-BruteForce3/src/Solution.java
@@ -4,6 +4,34 @@
  * 카펫
  * 
  */
-public class Solution {
+import java.lang.Math;
+import java.util.ArrayList;
+import java.util.List;
 
+class Solution {
+    
+    private boolean flag = false;
+    
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        List<Integer> arr = new ArrayList<>(); 
+        
+        for(int i = 1; i <= Math.sqrt(yellow); i++) {
+            for(int j = 1; j <= yellow; j++) {
+                if(i * j == yellow) {
+                    getResult(i, j, brown, answer);
+                    if(flag) return answer;
+                }
+            }
+        }
+        return answer;
+    }
+    
+    public void getResult(int horizon, int vertical, int brown, int[] answer) {
+        int formula = (horizon + 2) * 2 + (vertical*2);
+        if(formula == brown) {
+                answer[0] = vertical + 2;
+                answer[1] = horizon + 2;
+        }
+    }
 }
