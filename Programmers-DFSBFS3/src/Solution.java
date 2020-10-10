790394b3c4a12938496460cdb2e0e4dc2de39e16
YangTaeSung <tsy0668@gmail.com>
2020-10-11 01:14:46 +0900

    Fix code and comments final

--- Programmers-DFSBFS3/src/Solution.java
+++ Programmers-DFSBFS3/src/Solution.java
@@ -2,55 +2,48 @@
  * 
  * Programmers - DFSBFS3
  * 단어 변환 
+ * return answer == 51 ? 0 : answer; // return 할 때
+ * answer = (answer > count) ? count : answer; // 할당할 때 
+ * return count == 1 ? true : false; // return 할 때 
  * 
  */
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Collections;
-
 class Solution {
-    private static List<Integer> answers = new ArrayList<>();
+    int answer;
+    boolean[] check;
+    
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
-        boolean[] check = new boolean[words.length];
-        for(int i = 0; i < words.length; i++) {
-            check[i] = false;
-        }
         
-        bfs(begin, target, words, check, answer);
+        check = new boolean[words.length]; // false로 초기화
+        int count = 0;
+        answer = 51; // answer의 max값을 정해줘야 카운트랑 비교해서 더 작은게 answer로 들어갈 수 있어. words가 최대 50개니까 한 번씩 다 변환해도 50이 최대야
         
-        int min = 0;
-        if(answers.size()>0) min = Collections.min(answers);
-        System.out.println(min);
+        dfs(begin, target, words, 0);
         
-        return min;
+        return answer == 51 ? 0 : answer; // answer가 51이면 타겟과 같은 단어가 없는거
     }
     
-    public void bfs(String begin, String target, String[] words, boolean[] check, int answer) {
-        answer++;
-        if(target == begin) {
-            answers.add(answer);
+    public void dfs(String presentWord, String target, String[] words, int count) {
+        if(presentWord.equals(target)) {
+            answer = (answer > count) ? count : answer;
         }
-        int diffWord = 0;
-        // 한 글자씩 비교
+        
         for(int i = 0; i < words.length; i++) {
-            String word = words[i];
-            if(!check[i]) {
-                // 글자가 한 글자 다른지 체크
-                for(int j = 0; j < begin.length(); j++) {
-                    if(begin.charAt(j)!=word.charAt(j)) {
-                        diffWord++;
-                    }
-                    if(diffWord >= 2) break;
-                }
-            
-                if(diffWord == 1) {
-                    diffWord = 0;
-                    check[i] = true;
-                    bfs(word, target, words, check, answer);
-                }
+            if(!check[i] && wordOne(presentWord,words[i])) { // 단어가 한 개만 다른지 검사하고 이미 했던건지 검사하고 안한거면 체크 해주고 카운트 1 증가해서 현재 단어로 dfs 재귀
+                check[i] = true;
+                dfs(words[i], target, words, count+1);
+                check[i] = false;
             }
         }
-        answer--;
+    }
+    
+    // 단어가 한 개만 다른지 검사
+    public boolean wordOne(String presentWord, String compare) {
+        int count = 0;
+        for(int i = 0; i < presentWord.length(); i++) {
+            if(presentWord.charAt(i) != compare.charAt(i)) {
+                count++;
+            }
+        }
+        return count == 1 ? true : false;
     }
 }
